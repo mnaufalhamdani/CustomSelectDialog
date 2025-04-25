ededef72bb1ac8f8cefb95c031eab1f98a7dfb91
dani <dani@duakelinci.co.id>
2025-04-25 09:51:33 +0700

    upgrade AGP 8.9.2, kotlin 1.9.22, migrate Kotlin DSL, BuildSrc, Product Flavors

---
+++ buildSrc/src/main/java/Configs.kt
@@ -0,0 +1,12 @@
+import org.gradle.api.JavaVersion
+
+object Configs {
+    const val applicationId = "com.example.customselectdialog"
+    const val moduleId = "com.mnaufalhamdani.customselectdialog"
+    const val minSdk = 21
+    const val compileSdk = 34
+    const val targetSdk = 29
+    const val versionCode = 1
+    const val versionName = "25.4.1"
+    val javaVersion = JavaVersion.VERSION_17
+}
---
+++ customselectdialog/build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("maven-publish")
+}
+
+android {
+    namespace = Configs.moduleId
+    compileSdk = Configs.compileSdk
+
+    defaultConfig {
+        minSdk = Configs.minSdk
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Configs.javaVersion
+        targetCompatibility = Configs.javaVersion
+    }
+    kotlinOptions {
+        jvmTarget = Configs.javaVersion.toString()
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    //Dimen
+    implementation(libs.sdp)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            create<MavenPublication>("release") {
+                // Applies the component for the release build variant.
+                // NOTE : Delete this line code if you publish Native Java / Kotlin Library
+                from(components["release"])
+                groupId = PublishSetting.groupId
+                artifactId = PublishSetting.artifactId
+                version = PublishSetting.version
+            }
+        }
+    }
+}
