5826f42b772d0d2df40d2decd9df9a104b99d5be
dani <dani@duakelinci.co.id>
2025-04-25 13:48:17 +0700

    adjustment style list with gradation and gif loader

--- buildSrc/src/main/java/Configs.kt
+++ buildSrc/src/main/java/Configs.kt
@@ -7,6 +7,6 @@ object Configs {
     const val compileSdk = 34
     const val targetSdk = 29
     const val versionCode = 1
-    const val versionName = "25.4.2"
+    const val versionName = "25.4.3"
     val javaVersion = JavaVersion.VERSION_17
 }
